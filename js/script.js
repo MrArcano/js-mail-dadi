5d147af412504ca78e468a3eee048b3e00033891
MrArcano <salvatore-dario@hotmail.it>
2023-09-12 13:29:39 +0200

    [add]: script email

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const emailArray = [
+  "ciao@ciao.it",
+  "ciao1@ciao.it",
+  "ciao2@ciao.it",
+  "ciao3@ciao.it",
+  "ciao4@ciao.it",
+  "ciao5@ciao.it",
+  "ciao6@ciao.it",
+  "ciao7@ciao.it",
+]
+
+console.log(emailArray);
+
+const emailUser = prompt("Inserisci la tua email:");
+
+console.log(emailUser);
+
+let checkFlag = "false";
+
+// for
+for(let i = 0; i < emailArray.length; i++){
+  if(emailArray[i] === emailUser){
+    checkFlag = "true";
+  }
+}
+// end for
+
+if (checkFlag="true"){
+  console.log("L'email inserita si trova in elenco puoi entrare");
+}
+
