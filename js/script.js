5442cf993e1bf2325c16178389cbc558230cd568
MrArcano <salvatore-dario@hotmail.it>
2023-09-12 13:42:29 +0200

    [add]: script nRand

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// MAIL
+// -------------------------------------------------
 const emailArray = [
   "ciao@ciao.it",
   "ciao1@ciao.it",
@@ -9,13 +11,9 @@ const emailArray = [
   "ciao7@ciao.it",
 ]
 
-console.log(emailArray);
-
 const emailUser = prompt("Inserisci la tua email:");
-
-console.log(emailUser);
-
 let checkFlag = false;
+
 // for
 for(let i = 0; i < emailArray.length; i++){
   if(emailArray[i] === emailUser){
@@ -23,8 +21,30 @@ for(let i = 0; i < emailArray.length; i++){
   }
 }
 // end for
-
+console.log("Mail in elenco...");
+console.log(emailArray);
+console.log(emailUser);
 if (checkFlag){
   console.log("L'email inserita si trova in elenco puoi entrare");
+} 
+// -------------------------------------------------
+console.log("-----------------");
+console.log("Gioco dei dadi...");
+
+const firstNumRnd = Math.ceil(Math.random() * 6);
+const secondNumRnd = Math.ceil(Math.random() * 6);
+
+console.log("n1: " + firstNumRnd);
+console.log("n2: " + secondNumRnd);
+
+
+if (firstNumRnd == secondNumRnd){
+  console.log("Sono uguali");
+}else
+if (firstNumRnd > secondNumRnd){
+  console.log("Il primo numero vince");
+}else{
+  console.log("Il secondo numero vince");
 }
+
 
